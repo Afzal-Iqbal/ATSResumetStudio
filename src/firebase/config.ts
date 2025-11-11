30ed7dd471eda3df44dcd32f8bb5fc8f6139a75d
AFZAL iqbal <afzalxdreader@gmail.com>
2025-11-11 18:32:38 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-812925763-f0e31",
+  "appId": "1:529421077719:web:e3574f1daf6936ab127492",
+  "apiKey": "AIzaSyA7eGxRNnEAzmLKLj4tGYy8D0DT048TYIs",
+  "authDomain": "studio-812925763-f0e31.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "529421077719"
+};
